fe1f47ace45e1cda63131c989805362e568c8d2a
Julia Świdroń <juliaswidron@MacBook-Air-Julia.local>
2020-08-26 22:01:57 +0200

    added the burger menu

---
+++ script.js
@@ -0,0 +1,18 @@
+let burger = document.querySelector(".burger");
+let navig = document.querySelector("#navig");
+let logo = document.querySelector(".logo")
+let menu_open = false;
+
+burger.addEventListener("click", open_menu);
+
+function open_menu(){
+    if (menu_open == false){
+        navig.classList.add("open");
+        logo.classList.add("move");
+        menu_open = true;
+    }else{
+        navig.classList.remove("open");
+        logo.classList.remove("move");
+        menu_open = false;
+    }
+}
